90fdea5db5baada2ba94fd128ef90dff1e9bd647
GarryGaller <ggaller@mail.ru>
2021-10-01 22:11:57 +0300

    Fixed bugs

--- strings/duplicates.go
+++ strings/duplicates.go
@@ -1,118 +1,162 @@
 package strings
 
 func Deduplicate(arr *[]string) {
-    /*
-       Classic duplicate removal algorithm on sorted data
-       Data varies arr-place
-    */
+	/*
+	   Classic duplicate removal algorithm on sorted data
+	   Data varies arr-place
+	*/
 
-    if len(*arr) <= 1 {
-        return
-    }
+	if len(*arr) <= 1 {
+		return
+	}
 
-    j := 0
+	j := 0
 
-    for i := 1; i < len(*arr); i++ {
-        if (*arr)[j] != (*arr)[i] {
-            j++
-            (*arr)[j] = (*arr)[i]
-        }
-    }
-    *arr = (*arr)[:j+1]
-    return
+	for i := 1; i < len(*arr); i++ {
+		if (*arr)[j] != (*arr)[i] {
+			j++
+			(*arr)[j] = (*arr)[i]
+		}
+	}
+	*arr = (*arr)[:j+1]
+	return
 }
 
 func Deduplicate2(arr []string) (out int) {
-    /*
-       Дедуплицирует отсортированный массив на месте,
-       перемещая уникальные элементы в начало списка
-       Возвращает длину массива после дедупликации
-    */
+	/*
+	   Дедуплицирует отсортированный массив на месте,
+	   перемещая уникальные элементы в начало списка
+	   Возвращает длину массива после дедупликации
+	*/
 
-    if len(arr) <= 1 {
-        out = len(arr)
-        return
-    }
+	if len(arr) <= 1 {
+		out = len(arr)
+		return
+	}
 
-    for i := 1; i < len(arr); i++ {
-        if arr[i] != arr[out] {
-            out += 1
-            arr[out] = arr[i]
-        }
-    }
-    out += 1
-    return
+	for i := 1; i < len(arr); i++ {
+		if arr[i] != arr[out] {
+			out += 1
+			arr[out] = arr[i]
+		}
+	}
+	out += 1
+	return
 }
 
 func Deduplicate3(arr []string) (out []string) {
-    /*
-       Возвращает новый массив без дубликтов из переданного отсортированного
-       (из группы одинаковых элементов берется последний)
-    */
-    prev := arr[0]
+	/*
+	   Возвращает новый массив без дубликтов из переданного отсортированного
+	   (из группы одинаковых элементов берется последний)
+	*/
+	prev := arr[0]
 
-    for _, curr := range arr {
-        if prev != curr {
-            out = append(out, prev)
-            prev = curr
-        }
-    }
+	for _, curr := range arr {
+		if prev != curr {
+			out = append(out, prev)
+			prev = curr
+		}
+	}
 
-    out = append(out, prev)
-    return
+	out = append(out, prev)
+	return
 }
 
 func Deduplicate4(arr []string) (out []string) {
-    /*
-       Возвращает новый массив без дубликтов из переданного отсортированного
-       (из группы одинаковых элементов берется последний)
-    */
-    prev := arr[0]
+	/*
+	   Возвращает новый массив без дубликтов из переданного отсортированного
+	   (из группы одинаковых элементов берется последний)
+	*/
+	prev := arr[0]
 
-    for i := 1; i < len(arr); i++ {
-        curr := arr[i]
-        if prev != curr {
-            out = append(out, prev)
-            prev = curr
-        }
-    }
+	for i := 1; i < len(arr); i++ {
+		curr := arr[i]
+		if prev != curr {
+			out = append(out, prev)
+			prev = curr
+		}
+	}
 
-    out = append(out, prev)
-    return
+	out = append(out, prev)
+	return
 }
 
 func Duplicates(arr []string) (out []string) {
-    /*
-       Возвращает  массив повторяющихся элементов из отсортированного массива
-       Возвращает только один дубликат из группы
-    */
+	/*
+	   Возвращает  массив повторяющихся элементов из отсортированного массива
+	   Возвращает только один дубликат из группы
+	*/
 
-    prev := arr[0]
-    cnt := 1
+	cnt := -1
 
-    for i := 1; i < len(arr); i++ {
-        curr := arr[i]
-        if prev != curr {
-            prev = curr
-            cnt = 1
-        } else {
-            if cnt == 1 {
-                out = append(out, prev)
-            }
-            cnt += 1
-        }
-    }
-    return
+	for i := 1; i < len(arr); i++ {
+		if arr[i] == arr[i-1] && cnt == -1 {
+			out = append(out, arr[i])
+			cnt += 1
+		} else {
+			cnt = -1
+		}
+	}
+	return
 }
- 
-func Unique(arr []string) (out []string){
-    /*
-       Возвращает  массив уникальных (не имеющих дубликатов) элементов
-       из отсортированного массива
-    */
+
+func Duplicates1(arr []string) (out []string) {
+	/*
+	   Возвращает  массив повторяющихся элементов из отсортированного массива
+	   Возвращает только один дубликат из группы
+	*/
+
+	prev := arr[0]
+	cnt := -1
+
+	for i := 1; i < len(arr); i++ {
+		curr := arr[i]
+		if curr == prev {
+			if cnt == -1 {
+				out = append(out, prev)
+			}
+			cnt += 1
+		} else {
+			prev = curr
+			cnt = -1
+		}
+	}
+	return
+}
+
+func Duplicates2(arr []string) (out []string) {
+	/*
+	   Возвращает  массив повторяющихся элементов из отсортированного массива
+	   Возвращает только один дубликат из группы
+	*/
+
+	prev := arr[0]
+	cnt := -1
+
+	for i := 1; i < len(arr); i++ {
+		curr := arr[i]
+		if prev != curr {
+			prev = curr
+			cnt = -1
+		} else {
+			if cnt == -1 {
+				out = append(out, prev)
+			}
+			cnt += 1
+		}
+	}
+	return
+}
+
+func Unique(arr []string) (out []string) {
+	/*
+	   Возвращает  массив уникальных (не имеющих дубликатов) элементов
+	   из отсортированного массива
+	*/
 	N := len(arr)
-	for i:=0;i < N;i++{
-		if (i==0||arr[i]>arr[i-1]) && (i==(N-1)||arr[i]<arr[i+1])  {
+
+	for i := 0; i < N; i++ {
+		if (i == 0 || arr[i] > arr[i-1]) && (i == (N-1) || arr[i] < arr[i+1]) {
 			out = append(out, arr[i])
 		}
 	}
@@ -120,33 +164,31 @@ func Unique(arr []string) (out []string){
 
 }
 
-
-
 func Unique2(arr []string) (out []string) {
-    /*
-       Возвращает  массив уникальных (не имеющих дубликатов) элементов
-       из отсортированного массива
-    */
-    prev := arr[0]
-    cnt := 1
+	/*
+	   Возвращает  массив уникальных (не имеющих дубликатов) элементов
+	   из отсортированного массива
+	*/
+	prev := arr[0]
+	cnt := -1
 
-    for i := 1; i < len(arr); i++ {
-        curr := arr[i]
-        if prev != curr {
-            if cnt == 1 {
-                out = append(out, prev)
-            }
-            prev = curr
-            cnt = 1
-        } else {
-            cnt += 1
-        }
-    }
-    if cnt == 1 {
-        out = append(out, prev)
-    }
+	for i := 1; i < len(arr); i++ {
+		curr := arr[i]
+		if prev != curr {
+			if cnt == -1 {
+				out = append(out, prev)
+			}
+			prev = curr
+			cnt = -1
+		} else {
+			cnt += 1
+		}
+	}
+	if cnt == -1 {
+		out = append(out, prev)
+	}
 
-    return
+	return
 }
 
 // Варианты тех же функций с использованием доп. памяти
@@ -155,54 +197,54 @@ func Unique2(arr []string) (out []string) {
 // Не требуют сортировки
 
 func Counter(arr []string) (out map[string]int) {
-    /*The dictionary for counting occurrences */
-    out = make(map[string]int)
+	/*The dictionary for counting occurrences */
+	out = make(map[string]int)
 
-    for i := 0; i < len(arr); i++ {
-        out[arr[i]] += 1
-    }
+	for i := 0; i < len(arr); i++ {
+		out[arr[i]] += 1
+	}
 
-    return
+	return
 }
 
 func DeduplicateCounter(arr []string) (out []string) {
-    /*
-    Возвращает новый массив без дубликатов
-    Не требует сортировки
-    */
+	/*
+	   Возвращает новый массив без дубликатов
+	   Не требует сортировки
+	*/
 
-    counter := Counter(arr)
-    for k, _ := range counter {
-        out = append(out, k)
-    }
+	counter := Counter(arr)
+	for k, _ := range counter {
+		out = append(out, k)
+	}
 
-    return
+	return
 }
 
 func DuplicatesCounter(arr []string) (out []string) {
-    /* Возвращает новый массив из дубликатов (по одному из группы) */
+	/* Возвращает новый массив из дубликатов (по одному из группы) */
 
-    counter := Counter(arr)
-    for k, v := range counter {
-        if v > 1 {
-            out = append(out, k)
-        }
-    }
+	counter := Counter(arr)
+	for k, v := range counter {
+		if v > 1 {
+			out = append(out, k)
+		}
+	}
 
-    return
+	return
 }
 
 func UniqueCounter(arr []string) (out []string) {
-    /* Возвращает новый массив из уникальных
-       (не имеющих повторов в исходном массиве) элементов
-    */
+	/* Возвращает новый массив из уникальных
+	   (не имеющих повторов в исходном массиве) элементов
+	*/
 
-    counter := Counter(arr)
-    for k, v := range counter {
-        if v == 1 {
-            out = append(out, k)
-        }
-    }
+	counter := Counter(arr)
+	for k, v := range counter {
+		if v == 1 {
+			out = append(out, k)
+		}
+	}
 
-    return
+	return
 }
--- strings/strings_test.go
+++ strings/strings_test.go
@@ -1,84 +1,84 @@
 package strings
 
 import (
-    "fmt"
-    "sort"
-    "testing"
+	"fmt"
+	"sort"
+	"testing"
 )
 
 func BenchmarkReverseStrings(b *testing.B) {
 
-    data := make([]string, 100000)
-    for i := 0; i < 100000; i++ {
-        data = append(data, fmt.Sprint(i))
-    }
+	data := make([]string, 100000)
+	for i := 0; i < 100000; i++ {
+		data[i] = fmt.Sprint(i)
+	}
 
-    b.ResetTimer()
+	b.ResetTimer()
 
-    for i := 0; i < b.N; i++ {
-        ReverseStrings(&data)
-    }
+	for i := 0; i < b.N; i++ {
+		ReverseStrings(&data)
+	}
 }
 
 func BenchmarkReverseStrings2(b *testing.B) {
 
-    data := make([]string, 100000)
-    for i := 0; i < 100000; i++ {
-        data = append(data, fmt.Sprint(i))
-    }
+	data := make([]string, 100000)
+	for i := 0; i < 100000; i++ {
+		data[i] = fmt.Sprint(i)
+	}
 
-    b.ResetTimer()
+	b.ResetTimer()
 
-    for i := 0; i < b.N; i++ {
-        ReverseStrings2(&data)
-    }
+	for i := 0; i < b.N; i++ {
+		ReverseStrings2(&data)
+	}
 }
 
 func BenchmarkReverse(b *testing.B) {
 
-    data := RandomString(100000)
+	data := RandomString(100000)
 
-    b.ResetTimer()
+	b.ResetTimer()
 
-    b.Run("Reverse", func(b *testing.B) {
+	b.Run("Reverse", func(b *testing.B) {
 
-        for i := 0; i < b.N; i++ {
-            Reverse(data)
-        }
+		for i := 0; i < b.N; i++ {
+			Reverse(data)
+		}
 
-    })
+	})
 
-    b.Run("Reverse1", func(b *testing.B) {
+	b.Run("Reverse1", func(b *testing.B) {
 
-        for i := 0; i < b.N; i++ {
-            Reverse1(data)
-        }
+		for i := 0; i < b.N; i++ {
+			Reverse1(data)
+		}
 
-    })
+	})
 
-    b.Run("Reverse2", func(b *testing.B) {
+	b.Run("Reverse2", func(b *testing.B) {
 
-        for i := 0; i < b.N; i++ {
-            Reverse2(data)
-        }
+		for i := 0; i < b.N; i++ {
+			Reverse2(data)
+		}
 
-    })
+	})
 
-    b.Run("Reverse3", func(b *testing.B) {
+	b.Run("Reverse3", func(b *testing.B) {
 
-        for i := 0; i < b.N; i++ {
-            Reverse3(data)
-        }
+		for i := 0; i < b.N; i++ {
+			Reverse3(data)
+		}
 
-    })
+	})
 
-    b.Run("ReverseSimple", func(b *testing.B) {
+	b.Run("ReverseSimple", func(b *testing.B) {
 
-        for i := 0; i < b.N; i++ {
-            ReverseSimple(data)
-        }
+		for i := 0; i < b.N; i++ {
+			ReverseSimple(data)
+		}
 
-    })
+	})
 
 }
 
@@ -99,183 +99,215 @@ ok      github.com/GarryGaller/goalgo/strings   10.592s
 
 func BenchmarkDeduplicate(b *testing.B) {
 
-    arr := make([]string, 100000)
-    for i := 0; i < 100000; i++ {
-        arr = append(arr, RandomString(10))
-    }
+	N := 100000
+	arr := make([]string, N)
+	for i := 1; i < N-1; i++ {
+		arr[i] = RandomString(10)
+	}
+	arr[0] = "aaaaaaaaaa"
+	arr[N-1] = "aaaaaaaaaa"
 
-    b.ResetTimer()
+	b.ResetTimer()
 
-    b.Run("Deduplicate", func(b *testing.B) {
-        data := make([]string, len(arr))
-        copy(data, arr)
-        sort.Strings(data)
-        b.ResetTimer()
+	b.Run("Deduplicate", func(b *testing.B) {
+		data := make([]string, len(arr))
+		copy(data, arr)
+		sort.Strings(data)
+		b.ResetTimer()
 
-        for i := 0; i < b.N; i++ {
-            Deduplicate(&data)
-        }
+		for i := 0; i < b.N; i++ {
+			Deduplicate(&data)
+		}
 
-    })
+	})
 
-    b.Run("Deduplicate2", func(b *testing.B) {
-        data := make([]string, len(arr))
-        copy(data, arr)
-        sort.Strings(data)
-        b.ResetTimer()
+	b.Run("Deduplicate2", func(b *testing.B) {
+		data := make([]string, len(arr))
+		copy(data, arr)
+		sort.Strings(data)
+		b.ResetTimer()
 
-        for i := 0; i < b.N; i++ {
-            _ = Deduplicate2(data)
-        }
+		for i := 0; i < b.N; i++ {
+			_ = Deduplicate2(data)
+		}
 
-    })
+	})
 
-    b.Run("Deduplicate3", func(b *testing.B) {
-        data := make([]string, len(arr))
-        copy(data, arr)
-        sort.Strings(data)
-        b.ResetTimer()
+	b.Run("Deduplicate3", func(b *testing.B) {
+		data := make([]string, len(arr))
+		copy(data, arr)
+		sort.Strings(data)
+		b.ResetTimer()
 
-        for i := 0; i < b.N; i++ {
-            _ = Deduplicate3(data)
-        }
+		for i := 0; i < b.N; i++ {
+			_ = Deduplicate3(data)
+		}
 
-    })
+	})
 
-    b.Run("Deduplicate4", func(b *testing.B) {
-        data := make([]string, len(arr))
-        copy(data, arr)
-        sort.Strings(data)
-        b.ResetTimer()
+	b.Run("Deduplicate4", func(b *testing.B) {
+		data := make([]string, len(arr))
+		copy(data, arr)
+		sort.Strings(data)
+		b.ResetTimer()
 
-        for i := 0; i < b.N; i++ {
-            _ = Deduplicate4(data)
-        }
+		for i := 0; i < b.N; i++ {
+			_ = Deduplicate4(data)
+		}
 
-    })
+	})
 
-    b.Run("DeduplicateC", func(b *testing.B) {
-        data := make([]string, len(arr))
-        copy(data, arr)
-        b.ResetTimer()
+	b.Run("DeduplicateC", func(b *testing.B) {
+		data := make([]string, len(arr))
+		copy(data, arr)
+		b.ResetTimer()
 
-        for i := 0; i < b.N; i++ {
-            _ = DeduplicateCounter(data)
-        }
+		for i := 0; i < b.N; i++ {
+			_ = DeduplicateCounter(data)
+		}
 
-    })
+	})
 
 }
 
 // benchmarks
 //go test -bench=Deduplicate -benchmem
 /*
-BenchmarkDeduplicate/Deduplicate-4                   333           3459657 ns/op               0 B/op          0 allocs/op
-BenchmarkDeduplicate/Deduplicate2-4                  175           6920395 ns/op               0 B/op          0 allocs/op
-BenchmarkDeduplicate/Deduplicate3-4                   54          22242004 ns/op         9247348 B/op         30 allocs/op
-BenchmarkDeduplicate/Deduplicate4-4                   49          22184943 ns/op         9247353 B/op         30 allocs/op
-BenchmarkDeduplicate/DeduplicateC-4                   18          62059106 ns/op        17329089 B/op       3999 allocs/op
+
+BenchmarkDeduplicate/Deduplicate-4                   340           3467846 ns/op               0 B/op          0 allocs/op
+BenchmarkDeduplicate/Deduplicate2-4                  338           3470613 ns/op               0 B/op          0 allocs/op
+BenchmarkDeduplicate/Deduplicate3-4                   54          28834983 ns/op         9247364 B/op         30 allocs/op
+BenchmarkDeduplicate/Deduplicate4-4                   40          27551575 ns/op         9247361 B/op         30 allocs/op
+BenchmarkDeduplicate/DeduplicateC-4                   19          60582416 ns/op        17332395 B/op       4015 allocs/op
 PASS
-ok      github.com/GarryGaller/goalgo/strings   7.915s
+ok      github.com/GarryGaller/goalgo/strings   7.872s
+
+
 */
- 
 
 func BenchmarkDuplicates(b *testing.B) {
+	N := 100000
+	arr := make([]string, N)
+	for i := 1; i < N-1; i++ {
+		arr[i] = RandomString(10)
+	}
+	arr[0] = "aaaaaaaaaa"
+	arr[N-1] = "aaaaaaaaaa"
 
-    arr := make([]string, 100000)
-    for i := 0; i < 100000; i++ {
-        arr = append(arr, RandomString(10))
-    }
+	b.ResetTimer()
 
-    b.ResetTimer()
+	b.Run("Duplicates", func(b *testing.B) {
+		data := make([]string, len(arr))
+		copy(data, arr)
+		sort.Strings(data)
+		b.ResetTimer()
 
-    b.Run("Duplicates", func(b *testing.B) {
-        data := make([]string, len(arr))
-        copy(data, arr)
-        sort.Strings(data)
-        b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			_ = Duplicates(data)
+		}
 
-        for i := 0; i < b.N; i++ {
-            _ = Duplicates(data)
-        }
+	})
 
-    })
+	b.Run("Duplicates1", func(b *testing.B) {
+		data := make([]string, len(arr))
+		copy(data, arr)
+		sort.Strings(data)
+		b.ResetTimer()
 
-    b.Run("DuplicatesC", func(b *testing.B) {
-        data := make([]string, len(arr))
-        copy(data, arr)
-        b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			_ = Duplicates1(data)
+		}
 
-        for i := 0; i < b.N; i++ {
-            _ = DuplicatesCounter(data)
-        }
+	})
 
-    })
+	b.Run("Duplicates2", func(b *testing.B) {
+		data := make([]string, len(arr))
+		copy(data, arr)
+		sort.Strings(data)
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			_ = Duplicates2(data)
+		}
+
+	})
+
+	b.Run("DuplicatesC", func(b *testing.B) {
+		data := make([]string, len(arr))
+		copy(data, arr)
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			_ = DuplicatesCounter(data)
+		}
+
+	})
 }
 
 // benchmarks
 //go test -bench=Duplicates -benchmem
 /*
-BenchmarkDuplicates/Duplicates-4                     238           5021296 ns/op              16 B/op          1 allocs/op
-BenchmarkDuplicates/DuplicatesC-4                     26          45964165 ns/op         8088285 B/op       4001 allocs/op
+BenchmarkDuplicates/Duplicates-4                     372           3312017 ns/op              16 B/op          1 allocs/op
+BenchmarkDuplicates/Duplicates1-4                    327           3446680 ns/op              16 B/op          1 allocs/op
+BenchmarkDuplicates/Duplicates2-4                    354           3522801 ns/op              16 B/op          1 allocs/op
+BenchmarkDuplicates/DuplicatesC-4                     24          46794342 ns/op         8086087 B/op       3991 allocs/op
 PASS
-ok      github.com/GarryGaller/goalgo/strings   4.401s
+ok      github.com/GarryGaller/goalgo/strings   7.515s
 */
-
 
 func BenchmarkUnique(b *testing.B) {
 
-    arr := make([]string, 100000)
-    for i := 0; i < 100000; i++ {
-        arr = append(arr, RandomString(10))
-    }
+	N := 100000
+	arr := make([]string, N)
+	for i := 1; i < N-1; i++ {
+		arr[i] = RandomString(10)
+	}
+	arr[0] = "aaaaaaaaaa"
+	arr[N-1] = "aaaaaaaaaa"
+	b.ResetTimer()
 
-    b.ResetTimer()
+	b.Run("Unique", func(b *testing.B) {
+		data := make([]string, len(arr))
+		copy(data, arr)
+		sort.Strings(data)
+		b.ResetTimer()
 
-    b.Run("Unique", func(b *testing.B) {
-        data := make([]string, len(arr))
-        copy(data, arr)
-        sort.Strings(data)
-        b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			_ = Unique(data)
+		}
 
-        for i := 0; i < b.N; i++ {
-            _ = Unique(data)
-        }
+	})
 
-    }) 
-    
-    
-    b.Run("Unique2", func(b *testing.B) {
-        data := make([]string, len(arr))
-        copy(data, arr)
-        sort.Strings(data)
-        b.ResetTimer()
+	b.Run("Unique2", func(b *testing.B) {
+		data := make([]string, len(arr))
+		copy(data, arr)
+		sort.Strings(data)
+		b.ResetTimer()
 
-        for i := 0; i < b.N; i++ {
-            _ = Unique2(data)
-        }
+		for i := 0; i < b.N; i++ {
+			_ = Unique2(data)
+		}
 
-    })
-    
+	})
 
-    b.Run("UniqueC", func(b *testing.B) {
-        data := make([]string, len(arr))
-        copy(data, arr)
-        b.ResetTimer()
+	b.Run("UniqueC", func(b *testing.B) {
+		data := make([]string, len(arr))
+		copy(data, arr)
+		b.ResetTimer()
 
-        for i := 0; i < b.N; i++ {
-            _ = UniqueCounter(data)
-        }
+		for i := 0; i < b.N; i++ {
+			_ = UniqueCounter(data)
+		}
 
-    })
-}   
+	})
+}
 
 // benchmarks
-//go test -bench=Unique -benchmem 
+//go test -bench=Unique -benchmem
 /*
-BenchmarkUnique/Unique-4                      37          27542114 ns/op         9247357 B/op         30 allocs/op
-BenchmarkUnique/Unique2-4                     50          24141380 ns/op         9247346 B/op         30 allocs/op
-BenchmarkUnique/UniqueC-4                     16          64128669 ns/op        17336937 B/op       4037 allocs/op
+BenchmarkUnique/Unique-4                      42          30192202 ns/op         9247360 B/op         30 allocs/op
+BenchmarkUnique/Unique2-4                     56          27430139 ns/op         9247352 B/op         30 allocs/op
+BenchmarkUnique/UniqueC-4                     18          60559022 ns/op        17335775 B/op       4031 allocs/op
 PASS
-ok      github.com/GarryGaller/goalgo/strings   5.613s
+ok      github.com/GarryGaller/goalgo/strings   4.534s
 */
