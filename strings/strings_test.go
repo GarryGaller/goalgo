932ef2adbff801c9fbcaa10fb6edee1790d0f18f
GarryGaller <ggaller@mail.ru>
2021-10-01 20:17:02 +0300

    Added new function and test

--- strings/strings_test.go
+++ strings/strings_test.go
@@ -1,84 +1,84 @@
 package strings
 
 import (
-	"fmt"
-	"sort"
-	"testing"
+    "fmt"
+    "sort"
+    "testing"
 )
 
 func BenchmarkReverseStrings(b *testing.B) {
 
-	data := make([]string, 100000)
-	for i := 0; i < 100000; i++ {
-		data = append(data, fmt.Sprint(i))
-	}
+    data := make([]string, 100000)
+    for i := 0; i < 100000; i++ {
+        data = append(data, fmt.Sprint(i))
+    }
 
-	b.ResetTimer()
+    b.ResetTimer()
 
-	for i := 0; i < b.N; i++ {
-		ReverseStrings(&data)
-	}
+    for i := 0; i < b.N; i++ {
+        ReverseStrings(&data)
+    }
 }
 
 func BenchmarkReverseStrings2(b *testing.B) {
 
-	data := make([]string, 100000)
-	for i := 0; i < 100000; i++ {
-		data = append(data, fmt.Sprint(i))
-	}
+    data := make([]string, 100000)
+    for i := 0; i < 100000; i++ {
+        data = append(data, fmt.Sprint(i))
+    }
 
-	b.ResetTimer()
+    b.ResetTimer()
 
-	for i := 0; i < b.N; i++ {
-		ReverseStrings2(&data)
-	}
+    for i := 0; i < b.N; i++ {
+        ReverseStrings2(&data)
+    }
 }
 
 func BenchmarkReverse(b *testing.B) {
 
-	data := RandomString(100000)
+    data := RandomString(100000)
 
-	b.ResetTimer()
+    b.ResetTimer()
 
-	b.Run("Reverse", func(b *testing.B) {
+    b.Run("Reverse", func(b *testing.B) {
 
-		for i := 0; i < b.N; i++ {
-			Reverse(data)
-		}
+        for i := 0; i < b.N; i++ {
+            Reverse(data)
+        }
 
-	})
+    })
 
-	b.Run("Reverse1", func(b *testing.B) {
+    b.Run("Reverse1", func(b *testing.B) {
 
-		for i := 0; i < b.N; i++ {
-			Reverse1(data)
-		}
+        for i := 0; i < b.N; i++ {
+            Reverse1(data)
+        }
 
-	})
+    })
 
-	b.Run("Reverse2", func(b *testing.B) {
+    b.Run("Reverse2", func(b *testing.B) {
 
-		for i := 0; i < b.N; i++ {
-			Reverse2(data)
-		}
+        for i := 0; i < b.N; i++ {
+            Reverse2(data)
+        }
 
-	})
+    })
 
-	b.Run("Reverse3", func(b *testing.B) {
+    b.Run("Reverse3", func(b *testing.B) {
 
-		for i := 0; i < b.N; i++ {
-			Reverse3(data)
-		}
+        for i := 0; i < b.N; i++ {
+            Reverse3(data)
+        }
 
-	})
+    })
 
-	b.Run("ReverseSimple", func(b *testing.B) {
+    b.Run("ReverseSimple", func(b *testing.B) {
 
-		for i := 0; i < b.N; i++ {
-			ReverseSimple(data)
-		}
+        for i := 0; i < b.N; i++ {
+            ReverseSimple(data)
+        }
 
-	})
+    })
 
 }
 
@@ -99,71 +99,71 @@ ok      github.com/GarryGaller/goalgo/strings   10.592s
 
 func BenchmarkDeduplicate(b *testing.B) {
 
-	arr := make([]string, 100000)
-	for i := 0; i < 100000; i++ {
-		arr = append(arr, RandomString(10))
-	}
+    arr := make([]string, 100000)
+    for i := 0; i < 100000; i++ {
+        arr = append(arr, RandomString(10))
+    }
 
-	b.ResetTimer()
+    b.ResetTimer()
 
-	b.Run("Deduplicate", func(b *testing.B) {
-		data := make([]string, len(arr))
-		copy(data, arr)
-		sort.Strings(data)
-		b.ResetTimer()
+    b.Run("Deduplicate", func(b *testing.B) {
+        data := make([]string, len(arr))
+        copy(data, arr)
+        sort.Strings(data)
+        b.ResetTimer()
 
-		for i := 0; i < b.N; i++ {
-			Deduplicate(&data)
-		}
+        for i := 0; i < b.N; i++ {
+            Deduplicate(&data)
+        }
 
-	})
+    })
 
-	b.Run("Deduplicate2", func(b *testing.B) {
-		data := make([]string, len(arr))
-		copy(data, arr)
-		sort.Strings(data)
-		b.ResetTimer()
+    b.Run("Deduplicate2", func(b *testing.B) {
+        data := make([]string, len(arr))
+        copy(data, arr)
+        sort.Strings(data)
+        b.ResetTimer()
 
-		for i := 0; i < b.N; i++ {
-			_ = Deduplicate2(data)
-		}
+        for i := 0; i < b.N; i++ {
+            _ = Deduplicate2(data)
+        }
 
-	})
+    })
 
-	b.Run("Deduplicate3", func(b *testing.B) {
-		data := make([]string, len(arr))
-		copy(data, arr)
-		sort.Strings(data)
-		b.ResetTimer()
+    b.Run("Deduplicate3", func(b *testing.B) {
+        data := make([]string, len(arr))
+        copy(data, arr)
+        sort.Strings(data)
+        b.ResetTimer()
 
-		for i := 0; i < b.N; i++ {
-			_ = Deduplicate3(data)
-		}
+        for i := 0; i < b.N; i++ {
+            _ = Deduplicate3(data)
+        }
 
-	})
+    })
 
-	b.Run("Deduplicate4", func(b *testing.B) {
-		data := make([]string, len(arr))
-		copy(data, arr)
-		sort.Strings(data)
-		b.ResetTimer()
+    b.Run("Deduplicate4", func(b *testing.B) {
+        data := make([]string, len(arr))
+        copy(data, arr)
+        sort.Strings(data)
+        b.ResetTimer()
 
-		for i := 0; i < b.N; i++ {
-			_ = Deduplicate4(data)
-		}
+        for i := 0; i < b.N; i++ {
+            _ = Deduplicate4(data)
+        }
 
-	})
+    })
 
-	b.Run("DeduplicateC", func(b *testing.B) {
-		data := make([]string, len(arr))
-		copy(data, arr)
-		b.ResetTimer()
+    b.Run("DeduplicateC", func(b *testing.B) {
+        data := make([]string, len(arr))
+        copy(data, arr)
+        b.ResetTimer()
 
-		for i := 0; i < b.N; i++ {
-			_ = DeduplicateCounter(data)
-		}
+        for i := 0; i < b.N; i++ {
+            _ = DeduplicateCounter(data)
+        }
 
-	})
+    })
 
 }
 
@@ -182,35 +182,35 @@ ok      github.com/GarryGaller/goalgo/strings   7.915s
 
 func BenchmarkDuplicates(b *testing.B) {
 
-	arr := make([]string, 100000)
-	for i := 0; i < 100000; i++ {
-		arr = append(arr, RandomString(10))
-	}
+    arr := make([]string, 100000)
+    for i := 0; i < 100000; i++ {
+        arr = append(arr, RandomString(10))
+    }
 
-	b.ResetTimer()
+    b.ResetTimer()
 
-	b.Run("Duplicates", func(b *testing.B) {
-		data := make([]string, len(arr))
-		copy(data, arr)
-		sort.Strings(data)
-		b.ResetTimer()
+    b.Run("Duplicates", func(b *testing.B) {
+        data := make([]string, len(arr))
+        copy(data, arr)
+        sort.Strings(data)
+        b.ResetTimer()
 
-		for i := 0; i < b.N; i++ {
-			_ = Duplicates(data)
-		}
+        for i := 0; i < b.N; i++ {
+            _ = Duplicates(data)
+        }
 
-	})
+    })
 
-	b.Run("DuplicatesC", func(b *testing.B) {
-		data := make([]string, len(arr))
-		copy(data, arr)
-		b.ResetTimer()
+    b.Run("DuplicatesC", func(b *testing.B) {
+        data := make([]string, len(arr))
+        copy(data, arr)
+        b.ResetTimer()
 
-		for i := 0; i < b.N; i++ {
-			_ = DuplicatesCounter(data)
-		}
+        for i := 0; i < b.N; i++ {
+            _ = DuplicatesCounter(data)
+        }
 
-	})
+    })
 }
 
 // benchmarks
@@ -225,43 +225,57 @@ ok      github.com/GarryGaller/goalgo/strings   4.401s
 
 func BenchmarkUnique(b *testing.B) {
 
-	arr := make([]string, 100000)
-	for i := 0; i < 100000; i++ {
-		arr = append(arr, RandomString(10))
-	}
+    arr := make([]string, 100000)
+    for i := 0; i < 100000; i++ {
+        arr = append(arr, RandomString(10))
+    }
 
-	b.ResetTimer()
+    b.ResetTimer()
 
-	b.Run("Unique", func(b *testing.B) {
-		data := make([]string, len(arr))
-		copy(data, arr)
-		sort.Strings(data)
-		b.ResetTimer()
+    b.Run("Unique", func(b *testing.B) {
+        data := make([]string, len(arr))
+        copy(data, arr)
+        sort.Strings(data)
+        b.ResetTimer()
 
-		for i := 0; i < b.N; i++ {
-			_ = Duplicates(data)
-		}
+        for i := 0; i < b.N; i++ {
+            _ = Unique(data)
+        }
 
-	})
+    }) 
+    
+    
+    b.Run("Unique2", func(b *testing.B) {
+        data := make([]string, len(arr))
+        copy(data, arr)
+        sort.Strings(data)
+        b.ResetTimer()
 
-	b.Run("UniqueC", func(b *testing.B) {
-		data := make([]string, len(arr))
-		copy(data, arr)
-		b.ResetTimer()
+        for i := 0; i < b.N; i++ {
+            _ = Unique2(data)
+        }
 
-		for i := 0; i < b.N; i++ {
-			_ = UniqueCounter(data)
-		}
+    })
+    
 
-	})
+    b.Run("UniqueC", func(b *testing.B) {
+        data := make([]string, len(arr))
+        copy(data, arr)
+        b.ResetTimer()
+
+        for i := 0; i < b.N; i++ {
+            _ = UniqueCounter(data)
+        }
+
+    })
 }   
 
 // benchmarks
 //go test -bench=Unique -benchmem 
 /*
-BenchmarkUnique/Unique-4                     238           5012892 ns/op              16 B/op          1 allocs/op
-BenchmarkUnique/UniqueC-4                     16          62878600 ns/op        17333610 B/op       4021 allocs/op
+BenchmarkUnique/Unique-4                      37          27542114 ns/op         9247357 B/op         30 allocs/op
+BenchmarkUnique/Unique2-4                     50          24141380 ns/op         9247346 B/op         30 allocs/op
+BenchmarkUnique/UniqueC-4                     16          64128669 ns/op        17336937 B/op       4037 allocs/op
 PASS
-ok      github.com/GarryGaller/goalgo/strings   3.256s
+ok      github.com/GarryGaller/goalgo/strings   5.613s
 */
-
