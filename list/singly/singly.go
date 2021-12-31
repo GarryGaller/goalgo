00d16fc2ce03d688d5acbde353e7bfded9f710fa
GarryGaller <ggaller@mail.ru>
2021-12-31 15:13:31 +0300

    Added tests

--- list/singly/singly.go
+++ list/singly/singly.go
@@ -1,134 +1,139 @@
 package singlylinkedlist
 
 type Node struct {
-	value interface{}
-	next  *Node
+    value interface{}
+    next  *Node
 }
 
 type LinkedList struct {
-	head *Node
-	size int
+    head *Node
+    size int
 }
 
 func New(values ...interface{}) *LinkedList {
-	list := &LinkedList{}
-	if len(values) > 0 {
-		list.Add(values...)
-	}
-	return list
+    list := &LinkedList{}
+    if len(values) > 0 {
+        list.Add(values...)
+    }
+    return list
 }
 
 func (n *Node) Next() *Node {
-	return n.next
+    return n.next
 }
 
 func (n *Node) Value() interface{} {
-	return n.value
+    return n.value
 }
 
 func (l *LinkedList) Add(values ...interface{}) {
 
-	for _, value := range values {
-		l.PushBack(value)
-	}
+    for _, value := range values {
+        l.PushBack(value)
+    }
 }
 
 func (l *LinkedList) Append(values ...interface{}) {
-	l.Add(values...)
+    l.Add(values...)
 }
 
 func (l *LinkedList) First() *Node {
-	return l.head
+    return l.head
 }
 
 func (l *LinkedList) Front() *Node {
-	return l.head
+    return l.head
 }
 
 func (l *LinkedList) Get(index int) *Node {
-	idx := 0
-	for n := l.head; n != nil; n = n.next {
-		if idx == index {
-			return n
-		}
-		idx++
-	}
+    idx := 0
+    for n := l.head; n != nil; n = n.next {
+        if idx == index {
+            return n
+        }
+        idx++
+    }
 
-	return nil
+    return nil
 }
 
 func (l *LinkedList) Find(v interface{}) *Node {
-	for n := l.head; n != nil; n = n.next {
-		if n.value == v {
-			return n
-		}
-	}
-	return nil
+    for n := l.head; n != nil; n = n.next {
+        if n.value == v {
+            return n
+        }
+    }
+    return nil
 }
 
 func (l *LinkedList) Len() int {
-	return l.size
+    return l.size
 }
 
 func (l *LinkedList) IsEmpty() bool {
-	return l.size == 0
+    return l.size == 0
 }
 
 func (l *LinkedList) Clear() {
 
-	l.size = 0
-	l.head = nil
+    l.size = 0
+    l.head = nil
 }
 
 func (l *LinkedList) Has(v interface{}) bool {
 
-	return l.Find(v) != nil
+    return l.Find(v) != nil
 }
 
-func (l *LinkedList) PushBack(val interface{}) {
-	node := &Node{}
-	node.value = val
-
-	if l.size == 0 {
-		l.head = node
-		l.size++
-		return
-	}
-	head := l.head
-	for i := 0; i < l.size; i++ {
-		if head.next == nil {
-			head.next = node
-			l.size++
-			return
-		}
-		head = head.next
-	}
+func (l *LinkedList) PushBack(value interface{}) {
+    node := &Node{value:value}
+   
+    if l.size == 0 {
+        l.head = node
+        l.size++
+        return
+    }
+    head := l.head
+    for i := 0; i < l.size; i++ {
+        if head.next == nil {
+            head.next = node
+            l.size++
+            return
+        }
+        head = head.next
+    }
 }
+ 
+func (l *LinkedList) PushFront(value interface{}) {
+     node := &Node{value:value}
+     node.next = l.head
+     l.head = node
+}
+
 
 func (l *LinkedList) PopFront() *Node {
-	head := l.head
-	l.head = head.next
-	return head
+    head := l.head
+    l.head = head.next
+    return head
 }
 
 func (l *LinkedList) Reverse() {
 
-	var prev, next *Node
-	var curr = l.head
+    var prev, next *Node
+    var curr = l.head
 
-	for curr != nil {
-		next = curr.next
-		curr.next = prev
-		prev = curr
-		curr = next
-	}
-	l.head = prev
+    for curr != nil {
+        next = curr.next
+        curr.next = prev
+        prev = curr
+        curr = next
+    }
+    l.head = prev
 }
 
 //TODO:
 //IndexOf(value)
 //Remove(index)
 //Remove(node)
-//Has/Contains
 //Swap
 //Set(index, value)
--- list/singly/singly_test.go
+++ list/singly/singly_test.go
@@ -1,141 +1,183 @@
 package singlylinkedlist
 
 import (
-	"reflect"
-	"testing"
+    //"reflect"
+    "testing"
 )
 
 func TestNew(t *testing.T) {
 
-	got := New()
+    got := New()
 
-	if got == nil {
-		t.Errorf("list.New() = %v; want %v", got, New())
-	}
+    if got == nil {
+        t.Errorf("list.New() = %v; want %v", got, New())
+    }
 }
 
 func TestEmpty(t *testing.T) {
 
-	list := New()
-	got := list.IsEmpty()
+    list := New()
+    got := list.IsEmpty()
 
-	t.Run("For empty list", func(t *testing.T) {
-		if got != true {
-			t.Errorf("list.IsEmpty() = %v; want %v", got, true)
-		}
-	})
+    t.Run("For empty list", func(t *testing.T) {
+        if got != true {
+            t.Errorf("list.IsEmpty() = %v; want %v", got, true)
+        }
+    })
 
-	t.Run("For non empty list", func(t *testing.T) {
+    t.Run("For non empty list", func(t *testing.T) {
 
-		list = New(1, 2, 3)
-		list.Clear()
-		got = list.IsEmpty()
+        list = New(1, 2, 3)
+        list.Clear()
+        got = list.IsEmpty()
 
-		if got != true {
-			t.Errorf("list.IsEmpty() = %v; want %v", got, true)
-		}
+        if got != true {
+            t.Errorf("list.IsEmpty() = %v; want %v", got, true)
+        }
 
-	})
+    })
 
 }
 
 func TestAdd(t *testing.T) {
 
-	list := New()
-	list.Add(1, 2, 3)
+    list := New()
+    list.Add(0, 1, 2, 3)
 
-	got := list.Len()
-	want := 3
+    got := list.Len()
+    want := 4
 
-	if got != want {
-		t.Errorf("list.Len() = %v; want %v", got, want)
-	}
+    if got != want {
+        t.Errorf("list.Len() = %v; want %v", got, want)
+    }
 
 }
   
 
 func TestPopFrontAndFirst(t *testing.T) {
 
-	list := New()
-	list.Add(1, 2, 3)
+    list := New()
+    list.Add(0, 1, 2, 3)
 
-	node := list.PopFront()
-	got := node.value
-    want := 1
+    node := list.PopFront()
+    got := node.value
+    want := 0
 
-	if got != want {
-		t.Errorf("list.PopFront() = %v; want %v", got, want)
-	}
-    got = list.First().Value()
-    want = 2
     if got != want {
-		t.Errorf("list.First() = %v; want %v", got, want)
-	}
+        t.Errorf("list.PopFront() = %v; want %v", got, want)
+    }
+    got = list.First().Value()
+    want = 1
+    if got != want {
+        t.Errorf("list.First() = %v; want %v", got, want)
+    }
 
 }
+
+  
+func TestPushBack(t *testing.T) {
+
+    list := New()
+    for i:=0; i < 4; i++ {
+        list.PushBack(i)
+    }
+   
+    list2 := New()
+    list2.Add(0,1,2,3)
+    
+    for i := 0; i < list.size; i++ {
+        got, want := list.Get(i).Value(), list2.Get(i).Value()
+        if  got != want {
+            t.Errorf("list.PushBack() = %v; want %v", got, want)
+        }
+    }
+}
+  
+
+func TestPushFront(t *testing.T) {
+
+    list := New()
+    for i:=0; i < 4; i++ {
+        list.PushFront(i)
+    }
+   
+    list2 := New()
+    list2.Add(3, 2, 1, 0)
+    
+    for i := 0; i < list.size; i++ {
+        got, want := list.Get(i).Value(), list2.Get(i).Value()
+        if  got != want {
+            t.Errorf("list.PushFront() = %v; want %v", got, want)
+        }
+    }
+}
+
 
 
 func TestFind(t *testing.T) {
 
-	list := New()
+    list := New()
 
-	list.Add(0, 1, 2, 3)
-	for i := 0; i < list.Len(); i++ {
-		node := list.Find(i)
-		if node == nil {
-			t.Fatalf("list.Find(%d) = Node not found", i)
-		}
-		got := node.Value()
-		want := i
+    list.Add(0, 1, 2, 3)
+    for i := 0; i < list.Len(); i++ {
+        node := list.Find(i)
+        if node == nil {
+            t.Fatalf("list.Find(%d) = Node not found", i)
+        }
+        got := node.Value()
+        want := i
 
-		if got != want {
-			t.Errorf("list.Find(%d) = %v; want %v", got, want, i)
-		}
+        if got != want {
+            t.Errorf("list.Find(%d) = %v; want %v", got, want, i)
+        }
 
-	}
+    }
 }
 
 func TestGet(t *testing.T) {
 
-	list := New()
+    list := New()
 
-	list.Add(0, 1, 2, 3)
-	for i := 0; i < list.Len(); i++ {
-		node := list.Get(i)
-		if node == nil {
-			t.Fatalf("list.Get(%d) = Node not found", i)
-		}
-		got := node.Value()
-		want := i
+    list.Add(0, 1, 2, 3)
+    for i := 0; i < list.Len(); i++ {
+        node := list.Get(i)
+        if node == nil {
+            t.Fatalf("list.Get(%d) = Node not found", i)
+        }
+        got := node.Value()
+        want := i
 
-		if got != want {
-			t.Errorf("list.Get(%d) = %v; want %v", got, want, i)
-		}
+        if got != want {
+            t.Errorf("list.Get(%d) = %v; want %v", got, want, i)
+        }
 
-	}
+    }
 }
 
 func TestLen(t *testing.T) {
 
-	list := New()
-	list.Add(1, 2, 3)
+    list := New()
+    list.Add(1, 2, 3)
 
-	got := list.Len()
-	want := 3
-	if got != 3 {
-		t.Errorf("list.Len() = %v; want %v", got, want)
-	}
+    got := list.Len()
+    want := 3
+    if got != 3 {
+        t.Errorf("list.Len() = %v; want %v", got, want)
+    }
 }
 
 func TestReverse(t *testing.T) {
 
-	got := New()
-	got.Add(1, 2, 3)
-	got.Reverse()
-	want := New()
-	want.Add(3, 2, 1)
+    list := New()
+    list.Add(0, 1, 2, 3)
+    list.Reverse()
+    list2:= New()
+    list2.Add(3, 2, 1, 0)
 
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("list.Reverse() = %v; want %v", got, want)
-	}
+    for i := 0; i < list.size; i++ {
+        got, want := list.Get(i).Value(), list2.Get(i).Value()
+        if  got != want {
+            t.Errorf("list.Reverse() = %v; want %v", got, want)
+        }
+    }
 }
